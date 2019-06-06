cfa53b49166ee830861f889d88229287d9270c63
BL-CoderOvertime <brandonlively@live.com>
2019-06-06 19:04:43 -0400

    day4 project - Fixed yesterday's bug (freaking bracket in the wrong location), Heroku deployment set up, having a h10 issue i'm still working on.

--- src/main/kotlin/com/example/dog/DogsinitialApplication.kt
+++ src/main/kotlin/com/example/dog/DogsinitialApplication.kt
@@ -28,39 +28,7 @@ class DogsinitialApplication {
             var ctx: ApplicationContext = SpringApplication.run(DogsinitialApplication::class.java, *args)
             var dispatcherServlet: DispatcherServlet = ctx.getBean("dispatcherServlet") as DispatcherServlet
             dispatcherServlet.setThrowExceptionIfNoHandlerFound(true)
-
-            val logger = LoggerFactory.getLogger(DogsinitialApplication::class.java)
-            logger.info("Testing 123")
         }
-
-        const val EXCHANGE_NAME = "LambdaServer"
-        const val QUEUE_NAME_LOW = "LowPriorityQueue"
-        const val QUEUE_NAME_HIGH = "HighPriorityQueue"
-    }
-
-    @Bean
-    fun appExchange(): TopicExchange {
-        return TopicExchange(EXCHANGE_NAME)
-    }
-
-    @Bean
-    fun appQueueHigh(): Queue {
-        return Queue(QUEUE_NAME_HIGH)
-    }
-
-    @Bean
-    fun declareBindingHigh(): Binding {
-        return BindingBuilder.bind(appQueueHigh()).to(appExchange()).with(QUEUE_NAME_HIGH)
-    }
-
-    @Bean
-    fun appQueueLow(): Queue {
-        return Queue(QUEUE_NAME_LOW)
-    }
-
-    @Bean
-    fun declareBindingLow(): Binding {
-        return BindingBuilder.bind(appQueueLow()).to(appExchange()).with(QUEUE_NAME_LOW)
     }
 
     @Bean
--- src/main/kotlin/com/example/dog/controller/DogController.kt
+++ src/main/kotlin/com/example/dog/controller/DogController.kt
@@ -1,7 +1,6 @@
 package com.example.dog.controller
 
 import com.example.dog.CheckDog
-import com.example.dog.DogsinitialApplication
 import com.example.dog.exception.ResourceNotFoundException
 import com.example.dog.model.Dog
 import com.example.dog.ourDogList
@@ -11,15 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
-import com.example.dog.model.MessageDetail
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Autowired
 
 private val logger = LoggerFactory.getLogger(DogController::class.java)
 
-@Autowired
-var rt: RabbitTemplate? = null
 
 @RestController
 @RequestMapping("/dogs")
@@ -31,8 +25,6 @@ class DogController {
     fun getAllDogs(): ResponseEntity<*> {
         logger.info("/dogs/dogs accessed")
 
-        val message = MessageDetail("/dogs/dogs accessed", 7, false)
-        rt?.convertAndSend(DogsinitialApplication.QUEUE_NAME_LOW, message)
         return ResponseEntity(ourDogList.dogList, HttpStatus.OK)
     }
 
@@ -42,10 +34,6 @@ class DogController {
     fun getDogDetail(@PathVariable id: Long): ResponseEntity<*> {
 
         logger.trace("/dogs/$id accessed")
-
-        val message = MessageDetail("/dogs/$id accessed", 7, true)
-        rt?.convertAndSend(DogsinitialApplication.QUEUE_NAME_LOW, message)
-
 
         val rtnDog = ourDogList.findDog(object : CheckDog {
             override fun test(d: Dog): Boolean {
@@ -62,43 +50,38 @@ class DogController {
         }
         return ResponseEntity(rtnDog, HttpStatus.OK)
     }
-}
 
-// localhost:8080/dogs/breeds/{breed}
-@RequestMapping(value = "/breeds/{breed}")
-fun getDogBreeds(@PathVariable breed: String): ResponseEntity<*> {
 
-    logger.trace("/dogs/breeds/$breed accessed")
+    // localhost:8080/dogs/breeds/{breed}
+    @RequestMapping(value = "/breeds/{breed}")
+    fun getDogBreeds(@PathVariable breed: String): ResponseEntity<*> {
 
-    val message = MessageDetail("/dogs/breeds/$breed accessed", 7, true)
-    rt?.convertAndSend(DogsinitialApplication.QUEUE_NAME_HIGH, message)
+        logger.trace("/dogs/breeds/$breed accessed")
 
-    val rtnDogs = ourDogList.findDogs(object : CheckDog {
-        override fun test(d: Dog): Boolean {
-            return when (d.breed) {
-                breed -> true
-                else -> false
+        val rtnDogs = ourDogList.findDogs(object : CheckDog {
+            override fun test(d: Dog): Boolean {
+                return when (d.breed) {
+                    breed -> true
+                    else -> false
+                }
             }
+        })
+
+        if (rtnDogs.size == 0) {
+            throw ResourceNotFoundException("No Dogs of breed $breed found.")
         }
-    })
-
-    if (rtnDogs.size == 0) {
-        throw ResourceNotFoundException("No Dogs of breed $breed found.")
+        return ResponseEntity(rtnDogs, HttpStatus.OK)
     }
-    return ResponseEntity(rtnDogs, HttpStatus.OK)
-}
 
-@RequestMapping(value = "/dogtable")
-fun displayDogTable(): ModelAndView {
+    @RequestMapping(value = "/dogtable")
+    fun displayDogTable(): ModelAndView {
 
-    logger.trace("/dogs/dogtable accessed")
+        logger.trace("/dogs/dogtable accessed")
 
-    val message = MessageDetail("/dogs/dogtable accessed", 7, false)
-    rt?.convertAndSend(DogsinitialApplication.QUEUE_NAME_HIGH, message)
+        var mav = ModelAndView()
+        mav.viewName = "dogs"
+        mav.addObject("dogList", ourDogList.dogList)
 
-    var mav = ModelAndView()
-    mav.viewName = "dogs"
-    mav.addObject("dogList", ourDogList.dogList)
-
-    return mav
+        return mav
+    }
 }
